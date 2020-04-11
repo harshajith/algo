bb557c6e982a4bccadf5e49a5c3a799d8df60609
Harshajith <harsha.halgaswatta@gmail.com>
2020-04-11 12:03:28 +0800

    Add MinStack solution

--- src/main/java/leetcode30Challenge/HappyNumber.java
+++ src/main/java/leetcode30Challenge/HappyNumber.java
@@ -2,6 +2,23 @@ package leetcode30Challenge;
 
 import java.util.HashSet;
 
+/**
+ * Write an algorithm to determine if a number n is "happy".
+ *
+ * A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+ *
+ * Return True if n is a happy number, and False if not.
+ *
+ * Example:
+ *
+ * Input: 19
+ * Output: true
+ * Explanation:
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
+ */
 public class HappyNumber {
 
     public void printInSequence(int n){
@@ -11,12 +28,16 @@ public class HappyNumber {
             System.out.println(current % 10);
             current = current / 10;
         }
-
     }
 
 
-    public boolean isHappy1(int n){
-        HashSet<Integer> seen = new HashSet<>();
+    /**
+     * if n = 1234345, mod 10 gives the last number and remainder is rest of the numbers.
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n){
+        HashSet<Integer> seenSet = new HashSet<>();
         while(n != 1){
             int current = n;
             int sum = 0;
@@ -24,34 +45,19 @@ public class HappyNumber {
                 sum += (current % 10) * (current % 10);
                 current = current / 10;
             }
-            if(seen.contains(sum)){
+            if(seenSet.contains(sum)){
                 return false;
             }
 
-            seen.add(sum);
+            seenSet.add(sum);
             n = sum;
         }
         return true;
     }
 
 
-    public boolean isHappy(int n) {
-        String a = String.valueOf(n);
-        int sum = 0;
-        for(int i=0; i<a.length(); i++){
-            final int i1 = Integer.parseInt(a.substring(i, i + 1));
-            sum += i1 * i1;
-        }
-        if(sum == 1) {
-            return true;
-        }else {
-            return isHappy(sum);
-        }
-    }
-
     public static void main(String[] args){
         HappyNumber h = new HappyNumber();
-        //System.out.println(h.isHappy1(54));
-        h.printInSequence(1236784);
+        System.out.println(h.isHappy(32));
     }
 }
---
+++ src/test/java/MinStackTest.java
@@ -0,0 +1,22 @@
+import leetcode30Challenge.MinStack;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinStackTest {
+    MinStack minStack;
+    @Before
+    public void setUp(){
+        minStack = new MinStack();
+    }
+
+    @Test
+    public void testMinStackPush(){
+        minStack.push(-2);
+        minStack.push(-4);
+        minStack.push(2);
+        assert minStack.top() == 2;
+        minStack.pop();
+        assert minStack.getMin() == -4;
+    }
+
+}
