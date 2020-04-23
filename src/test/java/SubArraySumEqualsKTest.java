587c739910c4874b83fa9c532b77cb0ed0ac93d6
Harshajith <harsha.halgaswatta@gmail.com>
2020-04-23 09:25:14 +0800

    Add SubArraySumEqualsK, ConstructBinaryTreeFromPreOrderTraversal, LeftMostColumnWithLeastOnes

---
+++ src/test/java/SubArraySumEqualsKTest.java
@@ -0,0 +1,15 @@
+import leetcode.SubArraySumEqualsK;
+import org.junit.Test;
+
+public class SubArraySumEqualsKTest {
+
+    SubArraySumEqualsK subArraySumEqualsK = new SubArraySumEqualsK();
+
+    @Test
+    public void test1(){
+        int[] a = new int[]{1,1,1};
+        int result = subArraySumEqualsK.subarraySum(a, 2);
+        assert result == 2;
+    }
+
+}
