b6f89dc5aaf6e0eb4fdbc620e40472952540ac63
Harshajith <harsha.halgaswatta@gmail.com>
2020-04-28 22:39:20 +0800

    Add FirstUnique and MaximalSquare

---
+++ src/test/java/FirstUniqueTest.java
@@ -0,0 +1,20 @@
+import leetcode30Challenge.FirstUnique;
+import org.junit.Test;
+
+public class FirstUniqueTest {
+
+
+    @Test
+    public void test1(){
+
+        int[] input = new int[]{2,3,5};
+        FirstUnique firstUnique = new FirstUnique(input);
+        assert 2 == firstUnique.showFirstUnique();
+        firstUnique.add(5);
+        assert 2 == firstUnique.showFirstUnique();
+        firstUnique.add(2);
+        assert 3 == firstUnique.showFirstUnique();
+        firstUnique.add(3);
+        assert -1 == firstUnique.showFirstUnique();
+    }
+}
---
+++ src/test/java/MaximalSquareTest.java
@@ -0,0 +1,75 @@
+import leetcode30Challenge.MaximalSquare;
+import org.junit.Test;
+
+public class MaximalSquareTest {
+
+    MaximalSquare maximalSquare = new MaximalSquare();
+
+    @Test
+    public void test1(){
+        char[][] matrix = new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'},
+        };
+        int result = maximalSquare.maximalSquare(matrix);
+        assert result == 4;
+    }
+
+    @Test
+    public void test2(){
+        char[][] matrix = new char[][]{
+                {'1', '1'}
+        };
+        int result = maximalSquare.maximalSquare(matrix);
+        assert result == 1;
+    }
+
+    @Test
+    public void test3(){
+        char[][] matrix = new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'},
+        };
+        int result = maximalSquare.maximalSquareDynamicProgrammingModel(matrix);
+        assert result == 4;
+    }
+
+    @Test
+    public void test5(){
+        char[][] matrix = new char[][]{
+                {'0', '0', '0', '0', '0'},
+                {'1', '0', '0', '0', '0'},
+                {'0', '0', '0', '0', '1'},
+                {'0', '0', '0', '0', '0'},
+        };
+        int result = maximalSquare.maximalSquareDynamicProgrammingModel(matrix);
+        assert result == 1;
+    }
+
+//    @Test
+//    public void test6(){
+//        char[][] matrix = new char[][]{
+//                {'0', '0', '0', '1'},
+//                {'1', '1', '0', '1'},
+//                {'1', '1', '1', '1'},
+//                {'0', '1', '1', '1'},
+//                {'0', '1', '1', '1'}
+//        };
+//        int result = maximalSquare.maximalSquareDynamicProgrammingModel(matrix);
+//        assert result == 9;
+//    }
+//
+//
+//    @Test
+//    public void test4(){
+//        char[][] matrix = new char[][]{
+//                {'1', '1'}
+//        };
+//        int result = maximalSquare.maximalSquareDynamicProgrammingModel(matrix);
+//        assert result == 1;
+//    }
+}
